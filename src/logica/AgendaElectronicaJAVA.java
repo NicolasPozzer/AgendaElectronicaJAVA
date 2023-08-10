64c721c95f91acccad04d4e5d818574998c558af
NicolasPozzer <pozzernico@gmail.com>
2023-08-09 21:34:57 -0300

    publish-git

---
+++ src/logica/AgendaElectronicaJAVA.java
@@ -0,0 +1,19 @@
+package logica;
+
+import gui.Principal;
+
+
+public class AgendaElectronicaJAVA {
+
+
+    public static void main(String[] args) {
+        
+        Principal princ = new Principal();
+        
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
